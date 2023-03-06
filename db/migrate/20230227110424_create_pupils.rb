f7cfb7f6f38fb47365549fc62997158c6dcc5dc5
darlene9497 <117742892+darlene9497@users.noreply.github.com>
2023-03-06 15:23:42 +0300

    Update 20230227110424_create_pupils.rb

--- db/migrate/20230227110424_create_pupils.rb
+++ db/migrate/20230227110424_create_pupils.rb
@@ -5,7 +5,7 @@ class CreatePupils < ActiveRecord::Migration[6.1]
       t.integer :age
       t.string :gender
       t.string :parent_contact
-      t.string :teacher_name
+      t.integer :teacher_id
       t.string :stream_name
       t.integer :subject_id
     end
